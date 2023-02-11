03d13c85599438be1ab35690f09fab55113d2ef2
tarcisio <tarcisiomateus98@gmail.com>
2023-02-11 12:09:39 -0200

    created migrations/createNotes

---
+++ src/database/knex/migrations/20230211134755_createNotes.js
@@ -0,0 +1,14 @@
+exports.up = knex => knex.schema.createTable('notes', table => {
+    table.incremets('id')
+
+    table.text('title').notNullable()
+    table.text('description')
+
+    table.integer('user_id').references('id').inTable('users').onDelete('CASCADE')
+
+    table.timestamp('created_at').default(knex.fn.now())
+    table.timestamp('updated_at').default(knex.fn.now())
+})
+
+
+exports.down = knex => knex.schema.dropTable('notes')
