b04300441668bcedc1b11bd9669a2c060a8a4db1
tarcisio <tarcisiomateus98@gmail.com>
2023-02-12 14:09:45 -0200

    UsersController.js created method update{}

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,7 +2,7 @@ const knex = require('../database/knex')
 
 const appError = require('../utils/appError')
 
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 
 class UsersController {
     async create (request, response) {
@@ -19,6 +19,35 @@ class UsersController {
         await knex('users').insert({ name, email, password: cryptedPassword })
 
         return response.status(201).json()
+    }
+
+    async update (request, response) {
+        const { name, email, currentPassword, newPassword } = request.body
+        const { id } = request.params
+
+        const user = await knex('users').where({ id }).first()
+
+        const userWithEmail = await knex('users').where({ email }).first()
+        if (userWithEmail && userWithEmail.id !== user.id) {
+            throw new appError("this email it's already in use")
+        }
+
+        if (newPassword && !currentPassword) {
+            throw new appError("you must provide your current password if you want it to be updated")
+        }
+        const passwordCheck = await compare(currentPassword, user.password)
+        if (!passwordCheck) {
+            throw new appError("the given password it's wrong!")
+        } else {
+            user.password = await hash( newPassword, 8 )
+        }
+
+        user.name = name ?? user.name
+        user.email = email ?? user.email
+
+        await knex('users').where({ id }).update({ name: user.name, email: user.email, password: user.password})
+
+        return response.json()
     }
 }
 
