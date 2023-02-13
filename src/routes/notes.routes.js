b744c9c15174c2446e4a4b612bedc1531c150115
tarcisio <tarcisiomateus98@gmail.com>
2023-02-13 19:04:42 -0200

    created notes.routes.js

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require('express')
+
+const NotesController = require('../controllers/NotesController')
+const notesController = new NotesController()
+
+const notesRoutes = Router()
+
+notesRoutes.post('/', notesController.create)
+
+module.exports = notesRoutes
