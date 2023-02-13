fa31e9a80cb63d45b0af32717223bbb111b8cd90
tarcisio <tarcisiomateus98@gmail.com>
2023-02-13 14:10:20 -0200

    created routes/index.js

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@ const appError = require('./utils/appError')
 
 const express = require('express')
 
-const app = express()
+const routes = require('./routes')
 
+const app = express()
 app.use(express.json())
+
+app.use(routes)
 
 app.use(( error, request, response, next ) => {
     if (error instanceof appError) {
