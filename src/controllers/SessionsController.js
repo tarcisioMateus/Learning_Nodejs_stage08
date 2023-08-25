bcde2758956c407b526b17df0ace6a91480d6fbe
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-08-25 19:59:58 -0300

    installed jsonwebtoken

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,9 +1,12 @@
 const knex = require("../database/knex")
+const appError = require("../utils/appError")
+const { compare } = require("bcryptjs")
 const authConfig = require("../configs/auth")
+const { sign } = require("jsonwebtoken")
 
 class SessionsController {
   async create(request, response) {
-    
+
   }
 }
 
