4a2afc3e82d8607d200e726f88963368e7385533
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-08-25 19:53:54 -0300

    created SessionsController.js

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,10 @@
+const knex = require("../database/knex")
+const authConfig = require("../configs/auth")
+
+class SessionsController {
+  async create(request, response) {
+    
+  }
+}
+
+module.exports = SessionsController
