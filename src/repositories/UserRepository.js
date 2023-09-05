d817087500a14a1d6316d9040b2fdabb0752e48f
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-09-05 10:57:53 -0300

    UserRepository.js created findByEmail()

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -1,5 +1,13 @@
-class UserRepository {
+const knex = require('../database/knex')
 
+class UserRepository {
+    async findByEmail (email) {
+        const user = await knex('users').where({email}).first()
+
+        return user
+    }
+
+    
 }
 
 module.exports = UserRepository
