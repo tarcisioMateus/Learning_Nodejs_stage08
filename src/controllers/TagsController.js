621a0fab75b2010a9bf69f4f518cec3e293a6b2f
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-09-06 22:00:12 -0300

    updated TagsController.js

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -1,12 +1,17 @@
-const knex = require('../database/knex')
+const TagsRepository = require('../repositories/TagsRepository')
+
+const IndexServices = require('../services/tags/IndexServices')
 
 class TagsController {
+    tagsRepository = new TagsRepository()
+
     async index (request, response) {
         const user_id = request.user.id
 
-        let tags = await knex('tags').where({ user_id }).orderBy("name")
-        tags = tags.map( tag => tag.name )
-        tags = tagsOneEntryOnly ( tags )
+        const indexServices = new IndexServices({
+            tagsRepository: this.tagsRepository
+        })
+        const tags = await indexServices.execute({ user_id })
 
         return response.json(tags)
     }
