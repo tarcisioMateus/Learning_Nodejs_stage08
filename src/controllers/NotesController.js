de5a4b0d3089998a1352e8e176ad14564bf6b98c
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-09-06 21:34:42 -0300

    updated NotesController.js

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,42 +1,66 @@
-const knex = require('../database/knex')
+const NoteRepository = require('../repositories/NoteRepository')
+const TagsRepository = require('../repositories/TagsRepository')
+const LinksRepository = require('../repositories/LinksRepository')
 
-const appError = require('../utils/appError')
+const CreateServices = require('../services/note/CreateServices')
+const IndexServices = require('../services/note/IndexServices')
+const ShowServices = require('../services/note/ShowServices')
+const DeleteServices = require('../services/note/DeleteServices')
+const DeleteServices = require('../services/note/DeleteServices')
+
 
 class NotesController {
+    noteRepository = new NoteRepository()
+    tagsRepository = new TagsRepository()
+    linksRepository = new LinksRepository()
+
     async create (request, response) {
         const { title, description, tags, links } = request.body
         const user_id = request.user.id
 
-        
+        const createServices = new CreateServices({ 
+            noteRepository: this.noteRepository, 
+            tagsRepositor: this.tagsRepository, 
+            linksRepository: this.linksRepository 
+        })
+        const note = await createServices.execute({ title, description, tags, links, user_id })
 
-        return response.status(201).json()
+        return response.status(201).json(note)
     }
 
     async index (request, response) {
         const { title, tags } = request.query
         const user_id = request.user.id
 
+        const indexServices = new IndexServices({
+            noteRepository: this.noteRepository, 
+            tagsRepositor: this.tagsRepository
+        })
+        const notes = await indexServices.execute({ title, tags, user_id })
 
-
-        return response.json(notesWithTags)
+        return response.json(notes)
     }
 
     async show (request, response) {
         const { id } = request.params 
 
-
-
-        return response.json({
-            ...note,
-            tags,
-            links
+        const showServices = new ShowServices({ 
+            noteRepository: this.noteRepository, 
+            tagsRepositor: this.tagsRepository, 
+            linksRepository: this.linksRepository 
         })
+        const data = await showServices.execute({ id })
+
+        return response.json(data)
     }
 
     async delete (request, response) {
         const { id } = request.params
 
-        await knex('notes').where({ id }).delete()
+        const deleteServices = new DeleteServices({
+            noteRepository: this.noteRepository
+        })
+        await deleteServices.execute({ id })
 
         return response.json()
     }
--- src/repositories/TagsRepository.js
+++ src/repositories/TagsRepository.js
@@ -23,4 +23,4 @@ class TagsRepository {
   }
 }
 
-module.exports = TagRepository
+module.exports = TagsRepository
